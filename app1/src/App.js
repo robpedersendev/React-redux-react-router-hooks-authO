c28885d2be67634dd1d665980758854b45e3d07f
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-04-15 21:18:33 -0600

    Initiated react app, removed starter files

---
+++ app1/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  var name = "moe";
+  return (
+    <div className="App">
+      <header className="App-header">React{name}</header>
+    </div>
+  );
+}
+
+export default App;
